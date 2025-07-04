08ee804a54746300a1e7c018e6c59fbc42b42adc
Vambooze <52382335+Vambooze@users.noreply.github.com>
2025-07-04 11:23:17 -0500

    1

--- [so-scripts]/so-dumpster/fxmanifest.lua
+++ [so-scripts]/so-dumpster/fxmanifest.lua
@@ -5,7 +5,7 @@ author 'Shadow Ops'
 description 'Dumpter Hiding'
 version '1.0.0'
 
---shared_script 'config.lua' -- Load config file
+-- shared_script 'config.lua' -- Load config file
 client_script 'client.lua' -- Client-side script
 -- server_script 'server.lua' -- Server-side script
 
